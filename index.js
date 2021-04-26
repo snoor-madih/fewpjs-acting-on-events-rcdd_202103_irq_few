1d7e77499dd319a3c41134e13cae942f1e75510c
snoor_madih <snoor.madih@gmail.com>
2021-04-26 07:45:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ dodger.style.backgroundColor = "#FF69B4";
 document.addEventListener("keydown", function(e) {
   console.log(e.key);
 });
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
+// document.addEventListener("keydown", function(e) {
+//   if (e.key === "ArrowLeft") {
+//     let leftNumbers = dodger.style.left.replace("px", "");
+//     let left = parseInt(leftNumbers, 10);
  
-    dodger.style.left = `${left - 1}px`;
-  }
-});
+//     dodger.style.left = `${left - 1}px`;
+//   }
+// });
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
