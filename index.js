ac142a4cf235252878086de58528759888324fab
snoor_madih <snoor.madih@gmail.com>
2021-04-26 07:46:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function moveDodgerLeft() {
   }
 }
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(leftNumbers, 10);
  
   if (left > 0) {
     dodger.style.left = `${left + 1}px`;
