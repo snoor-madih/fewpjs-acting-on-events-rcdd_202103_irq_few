0bd7fbea63b4c165016985fc248ddec5ed921fc8
snoor_madih <snoor.madih@gmail.com>
2021-04-26 07:39:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "100px";
 dodger.style.bottom = "0px";
-document.addEventListener("keydown", function(e) {
-  console.log(e.key);
-});
+// document.addEventListener("keydown", function(e) {
+//   console.log(e.key);
+// });
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     let leftNumbers = dodger.style.left.replace("px", "");
