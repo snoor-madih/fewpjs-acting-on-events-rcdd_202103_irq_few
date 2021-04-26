5e2d2ec92d1ca1267b18b691848aa0a592d58c9a
snoor_madih <snoor.madih@gmail.com>
2021-04-26 07:40:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
-dodger.style.bottom = "100px";
-dodger.style.bottom = "0px";
-document.addEventListener("keydown", function(e) {
-  console.log(e.key);
-});
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
+// dodger.style.bottom = "100px";
+// dodger.style.bottom = "0px";
+// document.addEventListener("keydown", function(e) {
+//   console.log(e.key);
+// });
+// document.addEventListener("keydown", function(e) {
+//   if (e.key === "ArrowLeft") {
+//     let leftNumbers = dodger.style.left.replace("px", "");
+//     let left = parseInt(leftNumbers, 10);
  
-    dodger.style.left = `${left - 1}px`;
-  }
-});
+//     dodger.style.left = `${left - 1}px`;
+//   }
+// });
