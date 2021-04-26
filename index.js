a672b923b63f12aade383eb6b19f801e1ed8cbdd
snoor_madih <snoor.madih@gmail.com>
2021-04-26 07:44:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
-dodger.style.bottom = "100px";
-dodger.style.bottom = "0px";
+// dodger.style.bottom = "100px";
+// dodger.style.bottom = "0px";
 document.addEventListener("keydown", function(e) {
   console.log(e.key);
 });
