6557daaf37238446b0a4fe0ac84fb541db628480
snoor_madih <snoor.madih@gmail.com>
2021-04-26 07:42:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
