93d48cc07711043a5f9b2c9d1c2031e2537b2420
snoor_madih <snoor.madih@gmail.com>
2021-04-26 07:40:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ dodger.style.backgroundColor = "#FF69B4";
 //     dodger.style.left = `${left - 1}px`;
 //   }
 // });
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
