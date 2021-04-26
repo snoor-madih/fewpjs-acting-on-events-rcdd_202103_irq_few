0022893af726d0afc4fcedeabb6aa6037d868cff
snoor_madih <snoor.madih@gmail.com>
2021-04-26 07:37:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "100px";
 dodger.style.bottom = "0px";
+document.addEventListener("keydown", function(e) {
+  console.log(e.key);
+});
