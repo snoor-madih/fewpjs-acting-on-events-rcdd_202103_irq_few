e482681e99c989f04926e9b06a86f0c7cadd2d4b
snoor_madih <snoor.madih@gmail.com>
2021-04-26 07:47:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ function moveDodgerLeft() {
 }
 function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(leftNumbers, 10);
+  let right = parseInt(rightNumbers, 10);
  
-  if (left > 0) {
-    dodger.style.left = `${left + 1}px`;
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`;
   }
 }
 document.addEventListener("keydown", function(e) {
