2601dc889479cc64dd5a82a5fa928349904840b6
snoor_madih <snoor.madih@gmail.com>
2021-04-26 07:54:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.bottom = "100px";
+// dodger.style.bottom = "0px";
+document.addEventListener("keydown", function(e) {
+  console.log(e.key);
+});
+// document.addEventListener("keydown", function(e) {
+//   if (e.key === "ArrowLeft") {
+//     let leftNumbers = dodger.style.left.replace("px", "");
+//     let left = parseInt(leftNumbers, 10);
+ 
+//     dodger.style.left = `${left - 1}px`;
+//   }
+// });
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(rightNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
